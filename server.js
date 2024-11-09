261fc5003302b0b433f98aa630943cc11417df82
Artem Kuznetsov <papakarlo9822@gmail.com>
2024-11-09 16:35:54 +0100

    apiKey moved to the server side

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 3000;
 const apiKey = process.env.apiKey;
 
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
